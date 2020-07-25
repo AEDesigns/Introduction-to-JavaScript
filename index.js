be47ff1cb4ef5332c39bb4ad24b93d13bb902bd0
aedevdesigns@gmail.com <aedevdesigns@gmail.com>
2020-07-24 20:20:07 -0500

    Finish dog years function

--- index.js
+++ index.js
@@ -43,7 +43,9 @@ function multiplyNums(a, b){
 //Age in Dog years
 //write a function that takes your age and returns it to you in dog years - they say that 1 human year is equal to seven dog years 
 
-
+function ageToDogYears(age){
+    return age * 7
+}
 
 
 
